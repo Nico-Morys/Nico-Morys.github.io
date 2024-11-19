6a6e6c569a3b540cd102191cde436eabffbb2de2
Nico Morys <158112660+Nico-Morys@users.noreply.github.com>
2024-11-18 20:57:57 -0600

    added 307

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fetch predictions for multiple events
-const events = ['ufc308', 'ufc309']; // Add your events here
+const events = ['ufc307','ufc308', 'ufc309']; // Add your events here
 
 events.forEach(event => {
     fetch(`${event}-predictions.json`)  // Assuming each event has its own JSON file
